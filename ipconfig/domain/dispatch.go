4899222370f4af3b03a10c68fdedfba5b88ddb06
akuowen <akuowen2023@gmail.com>
2024-04-10 23:41:11 +0800

    路由

--- ipconfig/domain/dispatch.go
+++ ipconfig/domain/dispatch.go
@@ -1,18 +1,21 @@
 package domain
 
-import "github.com/oim/ipconfig/source"
+import (
+	"github.com/oim/ipconfig/source"
+)
 
 var dp *Dispatcher
 
 func Init() {
 	dp = &Dispatcher{}
-	// 开携程 进行调度
+	// 开协程 进行调度
 	go func() {
 		eventChan := source.EventChan()
 		for event := range eventChan {
 			switch event.Type {
 			case source.AddNodeEvent:
 				//调度器 更新的 操作
+				Add(event)
 			case source.DelNodeEvent:
 				//调度器 删除的操作
 			}
@@ -22,4 +25,15 @@ func Init() {
 }
 
 type Dispatcher struct {
+	local map[string]*Point
+}
+
+func Add(event *source.Event) {
+	point, ok := dp.local[event.Key()]
+	if !ok {
+		newPoint := NewPoint(event.Ip, event.Port)
+		dp.local[event.Key()] = newPoint
+	}
+	point.UpdateStat(NewStatus(event.ConnectNum, event.MessageBytes))
+
 }
