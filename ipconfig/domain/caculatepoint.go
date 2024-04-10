baf8ddb2337fd2e57cea9a688628d311acae6cbd
akuowen <akuowen2023@gmail.com>
2024-04-10 23:41:25 +0800

    计算

---
+++ ipconfig/domain/caculatepoint.go
@@ -0,0 +1,36 @@
+package domain
+
+import (
+	"sync/atomic"
+	"unsafe"
+)
+
+type Point struct {
+	Ip          string
+	Port        string
+	ActiveScore float64
+	StaticScore float64
+	Status      *Status
+	window      *window
+}
+
+func (ed *Point) UpdateStat(s *Status) {
+	ed.window.statusChan <- s
+}
+
+func NewPoint(ip, port string) *Point {
+	point := Point{
+		Ip:   ip,
+		Port: port,
+	}
+	point.window = newStateWindow()
+	point.Status = point.window.getStat()
+	go func() {
+		for status := range point.window.statusChan {
+			point.window.appendStat(status)
+			newStat := point.window.getStat()
+			atomic.SwapPointer((*unsafe.Pointer)((unsafe.Pointer)(point.Status)), unsafe.Pointer(newStat))
+		}
+	}()
+	return &point
+}
