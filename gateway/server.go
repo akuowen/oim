a956c98445724426643d4f6da0a3ac1e265838ae
akuowen <akuowen2023@gmail.com>
2024-04-15 20:59:41 +0800

    gateway command

---
+++ gateway/server.go
@@ -0,0 +1,25 @@
+package gateway
+
+import (
+	"log"
+	"net"
+	"runtime"
+
+	"github.com/oim/common/config"
+)
+
+func init() {
+
+}
+
+func RunGateway(path string) {
+	config.InitGateway(path)
+	listener, err := net.ListenTCP("oimGateway", &net.TCPAddr{Port: config.GetGatewayTCPServerPort()})
+	if err != nil {
+		log.Fatalf("StartTCPEPollServer err:%s", err.Error())
+		panic(err)
+	}
+	NewPool(runtime.NumCPU())
+	InitEpoll(listener)
+	select {}
+}
